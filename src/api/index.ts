35f2d56ad58b5ca2f89df8429e89c7133205e18c
JakeRSchwartz <jakerschwartz47@gmail.com>
2024-09-13 11:14:57 -0500

    vercel is not good

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api', ResumeRoutes)
-app.get('/favicon.ico', (req, res) => res.status(204))
+app.get('/favicon.png', (req, res) => res.status(204))
 
 
 export default app
